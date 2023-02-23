35450b9df761ad838eeba624629185d9655763ee
youngho.noh <mybrown04@gmail.com>
2023-02-23 17:11:23 +0900

    service account StacksetExecutionRole add

--- src/lib/stac-sets/stackset-stack.ts
+++ src/lib/stac-sets/stackset-stack.ts
@@ -5,6 +5,8 @@ import { StacksetConfig } from './03-config';
 import { StacksetCloudtrail } from './04-cloudtrail';
 import { StacksetGuarddutyMemberRole } from './05-guardduty-member';
 //import { PasswordPolicy } from '../stac-sets/02-password-policy';
+import { envVars } from '../config';
+import { StacksetExecutionRoleConstruct } from '../service-accounts/stackset-execution-role-construct';
 
 export interface StacksetStackProps extends cdk.StackProps {
 
@@ -13,6 +15,14 @@ export interface StacksetStackProps extends cdk.StackProps {
 export class StacksetStack extends cdk.Stack {
   constructor(scope: cdk.Construct, id: string, props: StacksetStackProps) {
     super(scope, id, props);
+
+    envVars.SERVICE_ACCOUNTS.forEach(account => {
+      if (account.Id == `${envVars.MASTER.ACCOUNT_ID}`
+      || account.Id == `${envVars.LOG_ARCHIVE.ACCOUNT_ID}`) {
+        return;
+      }
+      new StacksetExecutionRoleConstruct(this, 'StacksetExecutionRole', { stacksetRole: 'sub' });
+    });
 
     new StacksetAssumableRole(this, 'assumable-role');
 
