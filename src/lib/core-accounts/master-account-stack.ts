dfec5c56ef2efc9b1414807d3724c0e192105cc8
youngho.noh <mybrown04@gmail.com>
2023-02-23 13:55:26 +0900

    excution role test

--- src/lib/core-accounts/master-account-stack.ts
+++ src/lib/core-accounts/master-account-stack.ts
@@ -1,5 +1,5 @@
 import * as cdk from '@aws-cdk/core';
-//import { StacksetExecutionRoleConstruct } from '../service-accounts/stackset-execution-role-construct';
+import { StacksetExecutionRoleConstruct } from '../service-accounts/stackset-execution-role-construct';
 import { AssumeRoleConstruct } from './assume-role-construct';
 import { GuardDutyMainConstruct } from './guard-duty-main-construct';
 //import { GuardDutyMainConstruct } from './guard-duty-main-construct';
@@ -15,7 +15,7 @@ export class MasterAccountStack extends cdk.Stack {
     super(scope, id, props);
 
     //new LogArchiveConstruct(this, 'log-archive');
-    //new StacksetExecutionRoleConstruct(this, 'StackSetAdminRole', { stacksetRole: 'admin' } );
+    new StacksetExecutionRoleConstruct(this, 'StackSetAdminRole', { stacksetRole: 'admin' } );
 
     // env target account : log archive
     new IamGroupConstruct(this, 'iam-group');
